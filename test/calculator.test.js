6c4aec5e7c7c7f6de4847b14eef77bf035a4da2a
mgabor18 <56040199+mgabor18@users.noreply.github.com>
2024-12-02 16:56:01 +0100

    Add calculator code

---
+++ test/calculator.test.js
@@ -0,0 +1,37 @@
+const Calculator = require('../src/calculator');
+
+describe('Calculator', () => {
+  let calc;
+
+  beforeEach(() => {
+    calc = new Calculator(3);
+  });
+
+  it('should be able to give back the current value', () => {
+    throw new Error('Test is not implemented');
+  });
+
+  it('should be able to add a number to the current value', () => {
+    throw new Error('Test is not implemented');
+  });
+
+  it('should be able to subtract a number from the current value', () => {
+    throw new Error('Test is not implemented');
+  });
+
+  it('should be able to multiply the current value with a number', () => {
+    throw new Error('Test is not implemented');
+  });
+
+  it('should be able to divide the current value with a number', () => {
+    throw new Error('Test is not implemented');
+  });
+
+  it('should be able to get the modulo of the current value', () => {
+    throw new Error('Test is not implemented');
+  });
+
+  it('should be able to chain commands', () => {
+    throw new Error('Test is not implemented');
+  });
+});
